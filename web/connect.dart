0f2a33b86a0f05a2d02b2a452b9a647e86d8a51a
angekkk <github1997>
2020-11-02 17:43:52 +0100

    Test connection

---
+++ web/connect.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'package:sqljocky5/sqljocky.dart';
+import 'dart:html';
+
+
+
+/// Drops the tables if they already exist
+Future<void> dropTables(MySqlConnection conn) async {
+  print("Dropping tables ...");
+  await conn.execute("DROP TABLE IF EXISTS pets, people");
+  print("Dropped tables!");
+}
+
+
+Future<void> insertRows(MySqlConnection conn) async {
+
+}
+
+Future<void> readData(MySqlConnection conn) async {
+  Results result = await (await conn
+      .execute('SELECT * '
+      'FROM dataset '))
+      .deStream();
+  print(result);
+  print(result.map((r) => r.byName('nome')));
+
+}
+
+void main(){
+
+  querySelector('#out').text =  "CIAO";
+}
+
+void main2() async {
+  var s = ConnectionSettings(
+    user: "root",
+    password: "mysql",
+    host: "localhost",
+    port: 3306,
+    db: "pao",
+  );
+
+  // create a connection
+  print("Opening connection ...");
+  var conn = await MySqlConnection.connect(s);
+  print("Opened connection!");
+
+  await readData(conn);
+
+  await conn.close();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
